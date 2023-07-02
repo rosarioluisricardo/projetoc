e16a44a5ab5889c953d9ff51ad2519943a0d64a1
Luís Ricardo do Rosário Silva <lovewoodstar@gmail.com>
2023-07-02 14:52:36 -0300

    iniciando com linguagem C
    bem vindo a C

---
+++ fig02_01.c
@@ -0,0 +1,11 @@
+/*figura 2.1: Fig02_01.c
+primeiro programa em C */
+
+#include <stdio.h>
+
+/*A Função main indica o inicio do programa*/
+int main (void){
+	printf("Bem-Vindo a Linguagem C!\n");
+	
+	return 0; /* indica que o programa terminou com sucesso*/
+}/*fim da função main*/
