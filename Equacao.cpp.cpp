f2b87fbf5d2bd6da152f0ccc9180d8ee8514f0f3
Luís Ricardo do Rosário Silva <lovewoodstar@gmail.com>
2023-09-07 17:04:38 -0300

    codigos em c++

---
+++ Equacao.cpp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+	int x = 5;
+	int y = 6;
+	int sum = x + y;
+
+	std::cout << x << '\n';
+	std::cout << y << '\n';
+	std::cout << sum << '\n';
+
+	return 0;
+}
